97ce427b0d9b3476866357eadbc75da85d857159
resizens <60589904+resizens@users.noreply.github.com>
2020-03-23 22:37:34 +0700

    Add files via upload

---
+++ home.php
@@ -0,0 +1,31 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <a style ="display:block;height:100px;">
+      <table><tbody><tr><td style height="0px" width="140px">
+      <center><img src="categoria_img_1_20191215124153.png" alt="Slidebars Logo" height="90" width="90"></center>
+      </tr></tbody></table>
+    </a>  
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <h5 class="nav-link" href="#">About Us</h5>
+      </li>
+      <li class="nav-item active">
+        <h5 class="nav-link" href="login.php">Login</h5>
+      </li>
+    </ul>
+
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Cari" aria-label="Cari">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Cari</button>
+    </form>
+  
+   </div>
+   
+</nav>
+<img src="Cheetah-wallpapers-HD-pics-photos-download.jpg" height="800" width="1350">
